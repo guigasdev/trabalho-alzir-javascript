30624fe25d8077752ec23834608f339257229191
Guilherme <66035579+guigasdev@users.noreply.github.com>
2024-10-09 18:04:53 -0300

    Update fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -2,7 +2,7 @@
 let a = 0, b = 1, i = 2;
 let n = prompt("Digite a quantidade de termos: ");
 while(i<n){
-    System.out.println(a + ", " + b + ", ");
+    console.log(a + ", " + b + ", ");
     a = a+b;
     b = a+b;
     i = i+2;
